6c426235b4cf3ed0438f4f425ef82cf72361ae08
annnemariej <anne-marie@meireles.no>
2025-02-05 13:53:48 +0100

    Tegner noen platformer og spiller

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const canvas = document.createElement("canvas")
+const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
-canvas.width=600
-canvas.height=450
-document.body.appendChild(canvas)
+canvas.width = 600
 
-///Lager Spileren 
-
-
+// Lager spilleren
 const spiller = {
     x: 50,
     y: 300,
@@ -15,6 +11,30 @@ const spiller = {
     speed: 5,
     dy: 0,
     gravity: 0.5,
-    jumpPower: -10,
-    onGround: false
-  };
+    jumpPower: -10, 
+    onGround: false,
+    color: "blue", 
+}
+
+function tegneSpiller() {
+    ctx.fillStyle = spiller.color;
+    ctx.fillRect(spiller.x, spiller.y, spiller.width, spiller.height) 
+}
+tegneSpiller()
+
+ctx.lineWidth = 10
+
+
+//Tegner platformene
+function tegnLinje(startx, starty, stoppx, stoppy, farge) {
+    ctx.strokeStyle = farge
+    ctx.beginPath()
+    ctx.moveTo(startx, starty)
+    ctx.lineTo(stoppx, stoppy)
+    ctx.stroke()
+}
+
+tegnLinje(0, 200, 500, 350, "blue")
+tegnLinje(600, 200, 450, 250, "green")
+
+tegneSpiller()
